c4236e2cd77ba1fe08b536cf0500b9b34823372b
benjammin12 <benxerri@gmail.com>
2016-06-19 20:13:50 -0400

    prepend header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,26 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append(["Ben"]);
+ // $("#main").append(["Ben"]);
+
+
+var today = "Today is Sunday";
+
+var now = today.replace("Sunday", Date.now());
+
+
+$("#main").append(now);
+
+var formattedName = "Ben Xerri";
+var formattedRole = "Developer";
+
+var replaceName = HTMLheaderName.replace("%data%",formattedName);
+var replaceRole = HTMLheaderRole.replace("%data%",formattedRole);
+
+$("#main").prepend(replaceRole);
+$("#main").prepend(replaceName);
+
+
+
+
+
